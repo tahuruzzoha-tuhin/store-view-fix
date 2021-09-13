cfecbc22a91ab21fa65447a047ec97a581129531
tahuruzzoha-tuhin <tzz.tuhin@gmail.com>
2021-09-13 21:28:14 +0600

    [ADD] indentation

--- js/app.js
+++ js/app.js
@@ -13,16 +13,18 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+    div.innerHTML = `
+    <div class="single-product">
+        <div>
+          <img class="product-image" src=${image}></img>
+        </div>
+        <h3>${product.title}</h3>
+        <p>Category: ${product.category}</p>
+        <h2>Price: $ ${product.price}</h2>
+        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+        <button id="details-btn" class="btn btn-danger">Details</button>
+    </div>
+    `;
     document.getElementById("all-products").appendChild(div);
   }
 };
